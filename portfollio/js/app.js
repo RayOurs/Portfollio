d9c223b2f28626ec4044c4fef7172cb3c65bd1fa
RayOurs <135813383+RayOurs@users.noreply.github.com>
2024-06-18 02:32:50 +0200

    Delete portfollio directory

--- portfollio/js/app.js
+++
@@ -1,9 +0,0 @@
-function togglePopup(){
-    let popup = document.querySelector("#popup-overlay");
-    popup.classList.toggle("open");
-}
-
-function togglePopup2(){
-    let popup = document.querySelector("#popup-overlay2");
-    popup.classList.toggle("open");
-}
