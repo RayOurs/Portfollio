7c6281ee0e2315d85861d29828d76b46e91c0d01
RayOurs <135813383+RayOurs@users.noreply.github.com>
2024-06-18 02:34:02 +0200

    Add files via upload

---
+++ portfollio/js/app.js
@@ -0,0 +1,9 @@
+function togglePopup(){
+    let popup = document.querySelector("#popup-overlay");
+    popup.classList.toggle("open");
+}
+
+function togglePopup2(){
+    let popup = document.querySelector("#popup-overlay2");
+    popup.classList.toggle("open");
+}
